0e09476233bbaefad1ffb8dbfd9f84ffc3152625
J3r3mC <j.corbeilla@outlook.fr>
2026-07-17 20:06:45 +0200

    fix docker

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import * as core from "./core/index.js";
+import * as types from "./types/index.js";
 import { HttpServer } from "./api/http.server.js";
+
+export { core, types, HttpServer };
 
 const server = new HttpServer(3000);
 server.start();
